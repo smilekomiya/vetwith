eaa789672309312a2c6639183e7a56837bd947d0
yuzuyuzu <mwfots@yahoo.co.jp>
2015-02-06 16:01:42 +0900

    dbパスワード変更

--- dbsetting/db.php
+++ dbsetting/db.php
@@ -2,7 +2,7 @@
 /*データベースの接続設定*/
 $db['server'] = "localhost";
 $db['user'] = "root";
-$db['pass'] = "spitz538147";
+$db['pass'] = "1234";
 $db['dbname'] = "vetwith";
 
 /*MySQLデータベースに接続するケース*/
@@ -12,7 +12,7 @@ mysql_select_db($db['dbname'], $conn);
 //PDOクラスDSN
 $dsn = 'mysql:dbname=vetwith;host=localhost';
 $user = 'root';
-$password = 'spitz538147';
+$password = '1234';
 //データベースに接続
 try{
 	$dbh = new PDO($dsn, $user, $password);
